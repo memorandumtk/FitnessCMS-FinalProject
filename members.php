0a1b00a276a4c7d5e83da5f8b486c6ca63c9d002
memorandumtk <memorandum876@outlook.com>
2023-12-17 15:34:36 -0800

    merge kosuke2 and main

--- members.php
+++ members.php
@@ -19,21 +19,51 @@ switch($_SERVER["REQUEST_METHOD"]) {
                         echo("DB connection error ".$conn->connect_error);
                     } else {
                         $mid = $_POST["mid"];
-                        /* create a prepared statement */
-                        // Ref https://phpdelusions.net/mysqli_examples/update
-                        // We don't have to  confirm result by update clause.
+                        $output=[];
+                        // Select query for searching a member will be matched.
                         $sql = "SELECT * FROM `member_tb` WHERE `member_tb`.`mid` = ?";
                         $stmt= $conn->prepare($sql);
                         $stmt->bind_param("i", $mid);
                         $stmt->execute();
                         $result = $stmt->get_result();
                         $stmt->close();
-                        $output=[];
+                        $member=[];
                         if($result->num_rows === 1) {
-                            $output = $result->fetch_assoc();
-                            echo json_encode($output);
+                            $member = $result->fetch_assoc();
+                            array_push($output, $member);
                         } else {
                             echo "No such a user.";
+                        }
+                        // Select query for outputting all of instructors
+                        $sql2 = "SELECT * FROM `instructor_tb`";
+                        $stmt2= $conn->prepare($sql2);
+                        $stmt2->execute();
+                        $result2 = $stmt2->get_result();
+                        $stmt2->close();
+                        $instructors = [];
+                        if($result2->num_rows>0) {
+                            while($row = $result2->fetch_assoc()) {
+                                array_push($instructors, $row);
+                            }
+                            array_push($output, $instructors);
+                        } else {
+                            echo "No instructors.";
+                        }
+                        // Select query for outputting all of workouts
+                        $sql3 = "SELECT * FROM `workout_tb`";
+                        $stmt3= $conn->prepare($sql3);
+                        $stmt3->execute();
+                        $result3 = $stmt3->get_result();
+                        $stmt3->close();
+                        $workouts = [];
+                        if($result3->num_rows>0) {
+                            while($row = $result3->fetch_assoc()) {
+                                array_push($workouts, $row);
+                            }
+                            array_push($output, $workouts);
+                            echo json_encode($output);
+                        } else {
+                            echo "No workouts.";
                         }
                     }
                 } else {
@@ -52,13 +82,13 @@ switch($_SERVER["REQUEST_METHOD"]) {
                         $note = $_POST["note"];
                         $mid = $_POST["mid"];
                         $status = $_POST["status"];
-                        $instructors = $_POST["instructors"];
+                        $ifname = $_POST["instructors"];
                         /* create a prepared statement for UPDATE*/
                         // Ref https://phpdelusions.net/mysqli_examples/update
                         // We don't have to  confirm result by update clause.
-                        $sql = "UPDATE `member_tb` SET `status` = ?, `level` = ?, `days` = ?, `goal` = ?, `note` = ? WHERE `member_tb`.`mid` = ?";
+                        $sql = "UPDATE `member_tb` SET `status` = ?, `level` = ?, `days` = ?, `goal` = ?, `note` = ?, `ifname` = ? WHERE `member_tb`.`mid` = ?";
                         $stmt= $conn->prepare($sql);
-                        $stmt->bind_param("ssissi", $status, $level, $days, $goal, $note, $mid);
+                        $stmt->bind_param("ssisssi", $status, $level, $days, $goal, $note, $ifname, $mid);
                         $stmt->execute();
                         // Below is for passing user information using SELECT
                         $output=[];
@@ -76,7 +106,7 @@ switch($_SERVER["REQUEST_METHOD"]) {
                         // for searching instructor's information entered in request form.
                         $sql2 = "SELECT * FROM `instructor_tb` WHERE `fname` = ?";
                         $stmt2= $conn->prepare($sql2);
-                        $stmt2->bind_param("s", $instructors);
+                        $stmt2->bind_param("s", $ifname);
                         $stmt2->execute();
                         $result2 = $stmt2->get_result();
                         $stmt2->close();
@@ -92,7 +122,7 @@ switch($_SERVER["REQUEST_METHOD"]) {
                 }
                 break;
 
-            case "cards":
+            case "get-my-workouts":
                 if (isset($_POST["mid"])) {
                     if ($conn->connect_error) {
                         echo("DB connection error ".$conn->connect_error);
@@ -116,6 +146,28 @@ switch($_SERVER["REQUEST_METHOD"]) {
                     }
                 } else {
                     echo "This is invalid post request.";
+                }
+                break;
+            case "get-all-workouts":
+                if ($conn->connect_error) {
+                    echo("DB connection error ".$conn->connect_error);
+                } else {
+                    // $mid = $_POST["mid"];
+                    // Getting workout data.
+                    $output=[];
+                    $sql = "SELECT * FROM `workout_tb`";
+                    $stmt= $conn->prepare($sql);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $stmt->close();
+                    if($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            array_push($output, $row);
+                        }
+                        echo json_encode($output);
+                    } else {
+                        echo "No workout data.";
+                    }
                 }
                 break;
             default:
